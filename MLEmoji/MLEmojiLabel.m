9fb40c0a016578bb86f453fc0f91b8c7936ebeeb
molon <dudl@qq.com>
2014-05-21 13:40:44 +0800

    微信表情适配

--- MLEmoji/MLEmojiLabel.m
+++ MLEmoji/MLEmojiLabel.m
@@ -38,8 +38,11 @@ REGULAREXPRESSION(AtRegularExpression, @"@[\\u4e00-\\u9fa5\\w\\-]+")
 //@"#([^\\#|.]+)#"
 REGULAREXPRESSION_OPTION(PoundSignRegularExpression, @"#([\\u4e00-\\u9fa5\\w\\-]+)#", NSRegularExpressionCaseInsensitive)
 
-REGULAREXPRESSION(EmojiRegularExpression, @"\\[[a-zA-Z0-9\\u4e00-\\u9fa5]+\\]")
+//微信的表情符其实不是这种格式，这个格式的只是让人看起来更友好。。
+//REGULAREXPRESSION(EmojiRegularExpression, @"\\[[a-zA-Z0-9\\u4e00-\\u9fa5]+\\]")
 
+//@"/:[\\w:~!@$&*()|+<>',?-]{1,8}" , // @"/:[\x21-\x2E\x30-\x7E]{1,8}"
+REGULAREXPRESSION(SlashEmojiRegularExpression, @"/:[\x21-\x2E\x30-\x7E]{1,8}")
 
 const CGFloat kLineSpacing = 4.0;
 const CGFloat kAscentDescentScale = 0.25; //在这里的话无意义，高度的结局都是和宽度一样
@@ -105,6 +108,10 @@ static CGFloat widthCallback(void *refCon) {
  *  PS.已经在里面把init里的修改掉了
  */
 - (void)commonInit {
+    
+    //这个是用来生成plist时候用到
+//    [self initPlist];
+    
     self.userInteractionEnabled = YES;
     self.multipleTouchEnabled = NO;
     
@@ -244,7 +251,7 @@ static inline CGFloat TTTFlushFactorForTextAlignment(NSTextAlignment textAlignme
                 runBounds.origin.x = penOffset + xOffset;
                 runBounds.origin.y = origins[lineIndex].y;
                 runBounds.origin.y -= runDescent;
-
+                
                 UIImage *image = [UIImage imageNamed:imageName];
                 runBounds.origin.x += kEmojiMargin;
                 runBounds.origin.y -= kEmojiOriginYOffset; //稍微矫正下。
@@ -256,7 +263,7 @@ static inline CGFloat TTTFlushFactorForTextAlignment(NSTextAlignment textAlignme
         
         lineIndex++;
     }
-
+    
 }
 
 
@@ -267,7 +274,11 @@ static inline CGFloat TTTFlushFactorForTextAlignment(NSTextAlignment textAlignme
 - (NSMutableAttributedString*)mutableAttributeStringWithEmojiText:(NSString*)emojiText
 {
     //获取所有表情的位置
-    NSArray *emojis = [kEmojiRegularExpression() matchesInString:emojiText
+//    NSArray *emojis = [kEmojiRegularExpression() matchesInString:emojiText
+//                                                         options:NSMatchingWithTransparentBounds
+//                                                           range:NSMakeRange(0, [emojiText length])];
+
+    NSArray *emojis = [kSlashEmojiRegularExpression() matchesInString:emojiText
                                                          options:NSMatchingWithTransparentBounds
                                                            range:NSMakeRange(0, [emojiText length])];
     
@@ -283,12 +294,35 @@ static inline CGFloat TTTFlushFactorForTextAlignment(NSTextAlignment textAlignme
 		location = range.location + range.length;
         
 		NSString *emojiKey = [emojiText substringWithRange:range];
-		NSString *imageName = [[MLEmojiLabel emojiDictionary] objectForKey:emojiKey];
+        
+        //微信的表情没有结束符号,所以有可能会发现过长的只有头部才是表情的段，需要循环检测一次。微信最大表情特殊字符是8个长度，检测8次即可
+        NSDictionary *emojiDict = [MLEmojiLabel emojiDictionary];
+        
+        //如果当前获得key后面有多余的，这个需要记录下
+        NSMutableAttributedString *otherAppendStr = nil;
+        
+		NSString *imageName = emojiDict[emojiKey];
+        if (!imageName&&emojiKey.length>2) {
+            NSUInteger maxDetctIndex = emojiKey.length>8+2?8:emojiKey.length-2;
+            //从头开始检测是否有对应的
+            for (NSUInteger i=0; i<maxDetctIndex; i++) {
+//                NSLog(@"%@",[emojiKey substringToIndex:3+i]);
+                imageName = emojiDict[[emojiKey substringToIndex:3+i]];
+                if (imageName) {
+                    otherAppendStr  = [[NSMutableAttributedString alloc]initWithString:[emojiKey substringFromIndex:3+i]];
+                    break;
+                }
+            }
+        }
+        
 		if (imageName) {
 			// 这里不用空格，空格有个问题就是连续空格的时候只显示在一行
 			NSMutableAttributedString *replaceStr = [[NSMutableAttributedString alloc] initWithString:kEmojiReplaceCharacter];
 			NSRange __range = NSMakeRange([attrStr length], 1);
 			[attrStr appendAttributedString:replaceStr];
+            if (otherAppendStr) { //有其他需要添加的
+                [attrStr appendAttributedString:otherAppendStr];
+            }
             
 			// 定义回调函数
 			CTRunDelegateCallbacks callbacks;
@@ -305,17 +339,16 @@ static inline CGFloat TTTFlushFactorForTextAlignment(NSTextAlignment textAlignme
 			metrics->descent = metrics->ascent*kAscentDescentScale;
 			CTRunDelegateRef delegate = CTRunDelegateCreate(&callbacks, metrics);
 			[attrStr addAttribute:(NSString *)kCTRunDelegateAttributeName
-			               value:(__bridge id)delegate
-			               range:__range];
+                            value:(__bridge id)delegate
+                            range:__range];
 			CFRelease(delegate);
             
 			// 设置自定义属性，绘制的时候需要用到
 			[attrStr addAttribute:kCustomGlyphAttributeImageName
-			               value:imageName
-			               range:__range];
+                            value:imageName
+                            range:__range];
 		} else {
-			NSString *rSubStr = [emojiText substringWithRange:range];
-			NSMutableAttributedString *originalStr = [[NSMutableAttributedString alloc] initWithString:rSubStr];
+			NSMutableAttributedString *originalStr = [[NSMutableAttributedString alloc] initWithString:emojiKey];
 			[attrStr appendAttributedString:originalStr];
 		}
     }
@@ -408,28 +441,47 @@ didSelectLinkWithTextCheckingResult:(NSTextCheckingResult *)result;
 }
 
 
-#pragma mark - other 
-//- (void)initPlist
-//{
-//    NSString *testString = @"[微笑][撇嘴][色][发呆][得意][流泪][害羞][闭嘴][睡][大哭][尴尬][发怒][调皮][呲牙][惊讶][难过][酷][冷汗][抓狂][吐][偷笑][愉快][白眼][傲慢][饥饿][困][惊恐][流汗][憨笑][悠闲][奋斗][咒骂][疑问][嘘][晕][疯了][衰][骷髅][敲打][再见][擦汗][抠鼻][鼓掌][糗大了][坏笑][左哼哼][右哼哼][哈欠][鄙视][委屈][快哭了][阴险][亲亲][吓][可怜][菜刀][西瓜][啤酒][篮球][乒乓][咖啡][饭][猪头][玫瑰][凋谢][嘴唇][爱心][心碎][蛋糕][闪电][炸弹][刀][足球][瓢虫][便便][月亮][太阳][礼物][拥抱][强][弱][握手][胜利][抱拳][勾引][拳头][差劲][爱你][NO][OK][爱情][飞吻][跳跳][发抖][怄火][转圈][磕头][回头][跳绳][投降]";
-//    NSMutableArray *testArray = [NSMutableArray array];
-//    NSMutableDictionary *testDict = [NSMutableDictionary dictionary];
-//    [kEmojiRegularExpression() enumerateMatchesInString:testString options:0 range:NSMakeRange(0, testString.length) usingBlock:^(NSTextCheckingResult *result, __unused NSMatchingFlags flags, __unused BOOL *stop) {
-//        [testArray addObject:[testString substringWithRange:result.range]];
-//        [testDict setObject:[NSString stringWithFormat:@"Expression_%ld",testArray.count] forKey:[testString substringWithRange:result.range]];
-//    }];
-//    NSString *documentDir = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
-//    NSString *doc = [NSString stringWithFormat:@"%@/expression.plist",documentDir];
-//    NSLog(@"%@,length:%ld",doc,testArray.count);
-//    if ([testArray writeToFile:doc atomically:YES]) {
-//        NSLog(@"归档expression.plist成功");
-//    }
-//    doc = [NSString stringWithFormat:@"%@/expressionImage.plist",documentDir];
-//    if ([testDict writeToFile:doc atomically:YES]) {
-//        NSLog(@"归档到expressionImage.plist成功");
-//    }
-//    
-//
-//}
+#pragma mark - other
+- (void)initPlist
+{
+    NSString *testString = @"/::)/::~/::B/::|/:8-)/::</::$/::X/::Z/::'(/::-|/::@/::P/::D/::O/::(/::+/:--b/::Q/::T/:,@P/:,@-D/::d/:,@o/::g/:|-)/::!/::L/::>/::,@/:,@f/::-S/:?/:,@x/:,@@/::8/:,@!/:!!!/:xx/:bye/:wipe/:dig/:handclap/:&-(/:B-)/:<@/:@>/::-O/:>-|/:P-(/::'|/:X-)/::*/:@x/:8*/:pd/:<W>/:beer/:basketb/:oo/:coffee/:eat/:pig/:rose/:fade/:showlove/:heart/:break/:cake/:li/:bome/:kn/:footb/:ladybug/:shit/:moon/:sun/:gift/:hug/:strong/:weak/:share/:v/:@)/:jj/:@@/:bad/:lvu/:no/:ok/:love/:<L>/:jump/:shake/:<O>/:circle/:kotow/:turn/:skip/:oY";
+    NSMutableArray *testArray = [NSMutableArray array];
+    NSMutableDictionary *testDict = [NSMutableDictionary dictionary];
+    [kSlashEmojiRegularExpression() enumerateMatchesInString:testString options:0 range:NSMakeRange(0, testString.length) usingBlock:^(NSTextCheckingResult *result, __unused NSMatchingFlags flags, __unused BOOL *stop) {
+        [testArray addObject:[testString substringWithRange:result.range]];
+        [testDict setObject:[NSString stringWithFormat:@"Expression_%ld",testArray.count] forKey:[testString substringWithRange:result.range]];
+    }];
+    
+    NSString *documentDir = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+    NSString *doc = [NSString stringWithFormat:@"%@/expression.plist",documentDir];
+    NSLog(@"%@,length:%ld",doc,testArray.count);
+    if ([testArray writeToFile:doc atomically:YES]) {
+        NSLog(@"归档expression.plist成功");
+    }
+    doc = [NSString stringWithFormat:@"%@/expressionImage.plist",documentDir];
+    if ([testDict writeToFile:doc atomically:YES]) {
+        NSLog(@"归档到expressionImage.plist成功");
+    }
+    
+    //    NSString *testString = @"[微笑][撇嘴][色][发呆][得意][流泪][害羞][闭嘴][睡][大哭][尴尬][发怒][调皮][呲牙][惊讶][难过][酷][冷汗][抓狂][吐][偷笑][愉快][白眼][傲慢][饥饿][困][惊恐][流汗][憨笑][悠闲][奋斗][咒骂][疑问][嘘][晕][疯了][衰][骷髅][敲打][再见][擦汗][抠鼻][鼓掌][糗大了][坏笑][左哼哼][右哼哼][哈欠][鄙视][委屈][快哭了][阴险][亲亲][吓][可怜][菜刀][西瓜][啤酒][篮球][乒乓][咖啡][饭][猪头][玫瑰][凋谢][嘴唇][爱心][心碎][蛋糕][闪电][炸弹][刀][足球][瓢虫][便便][月亮][太阳][礼物][拥抱][强][弱][握手][胜利][抱拳][勾引][拳头][差劲][爱你][NO][OK][爱情][飞吻][跳跳][发抖][怄火][转圈][磕头][回头][跳绳][投降]";
+    //    NSMutableArray *testArray = [NSMutableArray array];
+    //    NSMutableDictionary *testDict = [NSMutableDictionary dictionary];
+    //    [kEmojiRegularExpression() enumerateMatchesInString:testString options:0 range:NSMakeRange(0, testString.length) usingBlock:^(NSTextCheckingResult *result, __unused NSMatchingFlags flags, __unused BOOL *stop) {
+    //        [testArray addObject:[testString substringWithRange:result.range]];
+    //        [testDict setObject:[NSString stringWithFormat:@"Expression_%ld",testArray.count] forKey:[testString substringWithRange:result.range]];
+    //    }];
+    //    NSString *documentDir = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+    //    NSString *doc = [NSString stringWithFormat:@"%@/expression.plist",documentDir];
+    //    NSLog(@"%@,length:%ld",doc,testArray.count);
+    //    if ([testArray writeToFile:doc atomically:YES]) {
+    //        NSLog(@"归档expression.plist成功");
+    //    }
+    //    doc = [NSString stringWithFormat:@"%@/expressionImage.plist",documentDir];
+    //    if ([testDict writeToFile:doc atomically:YES]) {
+    //        NSLog(@"归档到expressionImage.plist成功");
+    //    }
+    
+    
+}
 
 @end
--- MLEmojiLabel/ViewController.m
+++ MLEmojiLabel/ViewController.m
@@ -63,12 +63,14 @@
         _emojiLabel.lineBreakMode = NSLineBreakByWordWrapping;
         
         
-		[_emojiLabel setEmojiText:@"链接:http://baidu.com电话18120136012邮箱dudl@qq.com@某某某 ,###哈哈哈###[s]#,[可爱]啊是大三的@阿萨德.com@dsad@dad天使。。@天使asas @[吃惊]sds  #ds#as# @asd_sad @asd-fdsf  #_dsad_# #阿萨德撒-asd[asd]#  [ - ] [-][吃惊][吃惊][吃惊][吃惊]"];
+//		[_emojiLabel setEmojiText:@"链接:http://baidu.com电话18120136012邮箱dudl@qq.com@某某某 ,###哈哈哈###[s]#,[可爱]啊是大三的@阿萨德.com@dsad@dad天使。。@天使asas @[吃惊]sds  #ds#as# @asd_sad @asd-fdsf  #_dsad_# #阿萨德撒-asd[asd]#  [ - ] [-][吃惊][吃惊][吃惊][吃惊]"];
         
 //        [_emojiLabel setEmojiText:@"asd[可爱]dd[吃惊]asdas"];
         
 //        [_emojiLabel setEmojiText:@"微笑[微笑][白眼][白眼][白眼][白眼]微笑[愉快]---[冷汗][投降][抓狂][害羞]"];
 //        [_emojiLabel setEmojiText:@"[微笑]-[撇嘴]-[色]-[发呆][得意]-[流泪][害羞][闭嘴][睡][大哭][尴尬]-[发怒]-[调皮][呲牙][惊讶][难过][酷]-[冷汗]-[抓狂][吐][偷笑][愉快][白眼][傲慢][饥饿][困][惊恐][流汗][憨笑][悠闲][奋斗][咒骂][疑问][嘘][晕][疯了][衰][骷髅][敲打][再见][擦汗][抠鼻][鼓掌][糗大了][坏笑][左哼哼][右哼哼][哈欠][鄙视][委屈][快哭了][阴险][亲亲][吓][可怜][菜刀][西瓜][啤酒][篮球][乒乓][咖啡][饭][猪头][玫瑰][凋谢][嘴唇][爱心][心碎][蛋糕][闪电][炸弹][刀][足球][瓢虫][便便][月亮][太阳][礼物][拥抱][强][弱][握手][胜利][抱拳][勾引][拳头][差劲][爱你][NO][OK][爱情][飞吻][跳跳][发抖][怄火][转圈][磕头][回头][跳绳][投降]"];
+        
+        [_emojiLabel setEmojiText:@"撒/:dsad旦/::)sss/::~啊是大三的/::B/::|/:8-)/::</::$/::X/::Z/::'(/::-|/::@/::P/::D/::O/::(/::+/:--b/::Q/::T/:,@P/:,@-D/::d/:,@o/::g/:|-)/::!/::L/::>/::,@/:,@f/::-S/:?/:,@x/:,@@/::8/:,@!/:!!!/:xx/:bye/:wipe/:dig/:handclap/:&-(/:B-)/:<@/:@>/::-O/:>-|/:P-(/::'|/:X-)/::*/:@x/:8*/:pd/:<W>/:beer/:basketb/:oo/:coffee/:eat/:pig/:rose/:fade/:showlove/:heart/:break/:cake/:li/:bome/:kn/:footb/:ladybug/:shit/:moon/:sun/:gift/:hug/:strong/:weak/:share/:v/:@)/:jj/:@@/:bad/:lvu/:no/:ok/:love/:<L>/:jump/:shake/:<O>/:circle/:kotow/:turn/:skip/:oY"];
 	}
 	return _emojiLabel;
 }
